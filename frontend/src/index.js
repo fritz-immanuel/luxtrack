cd8946dafc72f8123f66c1224ad6544fe29494fa
Fritz Immanuel <fritz@atomic.id>
2025-10-05 21:56:02 +0700

    fix up build stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,8 @@
+if (typeof window !== 'undefined' && !window.$RefreshSig$) {
+  window.$RefreshSig$ = function() { return function(type) { return type; }; };
+  window.$RefreshReg$ = function() {};
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
